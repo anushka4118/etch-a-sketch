e44d4f8f334850c4828a1fbc8ac4c82a5c0bdc24
anushka4118 <anushka4118@yahoo.com>
2020-06-29 16:23:26 -0400

    Add random colour button

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const container = document.querySelector('.container');
 const cell = document.querySelector('cell')
 
 
-function makeGrid(gridSize) {
+function makeGrid(gridSize, colorChooser) {
     container.style.gridTemplateColumns =`repeat(${gridSize}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
 
@@ -11,23 +11,42 @@ function makeGrid(gridSize) {
         containerCell.className = 'cell';
 
         containerCell.addEventListener("mouseenter", function(){
-            containerCell.style.backgroundColor = 'black';
+            containerCell.style.backgroundColor = colorChooser();
         })
-
         container.appendChild(containerCell);
     }
 }
 
-makeGrid(16);
+makeGrid(16, function(){
+    return 'black';
+});
 
 function resetGrid() {
     container.innerHTML = '';
-    makeGrid(16);
+    makeGrid(16, function(){
+        return 'black';
+    });
 }
 
 function resizeGrid() {
     container.innerHTML = '';
     let newGridSize = prompt("What size would you like to set the grid to?");
-    makeGrid(newGridSize);
+    let color = prompt("What color would you like?");
+ 
+    makeGrid(newGridSize, function(){
+       return color;
+    });
 }
 
+function randomColor() {
+    container.innerHTML = '';
+
+    makeGrid(16, function(){
+        let r = Math.floor(Math.random() * 256);
+        let g = Math.floor(Math.random() * 256);
+        let b = Math.floor(Math.random() * 256);
+        let rgb = 'rgb(' + r + ',' + g + ',' + b + ')';
+
+        return rgb;
+    });
+}
